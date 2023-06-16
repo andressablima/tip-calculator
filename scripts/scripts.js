d36fcebf84eb0e91058e4e148118e38d7bde6eff
Andressa Lima <lima.andressab@outlook.com>
2023-06-16 09:25:44 -0300

    inicio do codigo em javascript

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,26 @@
-s
+
+
+function calculateTip(event) {
+     event.preventDefault();
+    let bill = document.getElementById('bill').value;
+    let tip = document.getElementById('tip-btn').value;
+    let numPeople = document.getElementById('people').value;
+
+    if(bill == '' | tip == 0){
+        alert("Preencha os valores")
+        return;
+    }
+
+    if(numPeople == "" | tip <= 1){
+        numPeople = 1;
+        document.getElementById('each').style.display = "none"
+    } else {
+        document.getElementById('each').style.display = "block"
+    }
+
+    let total = (bill * tip) / numPeople;
+    total = total.toFixed(2);
+
+
+
+}
