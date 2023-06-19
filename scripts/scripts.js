64d2f3958496329a093dd65b6c1ff025876a5c7c
Andressa Lima <lima.andressab@outlook.com>
2023-06-19 16:02:14 -0300

    ajustes html e css + js da calculadora

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,26 +1,72 @@
+const billInput = document.querySelector('.bill-input')
+const peopleInput = document.querySelector('.people-input')
+const tipPerPerson = document.getElementById('tip-amount')
+const totalPerPerson = document.getElementById('total-amount')
+const tips = document.querySelectorAll(".tips")
+const tipCustom = document.querySelector(".custom")
+const resetBtn = document.querySelector(".reset")
 
 
-function calculateTip(event) {
-     event.preventDefault();
-    let bill = document.getElementById('bill').value;
-    let tip = document.getElementById('tip-btn').value;
-    let numPeople = document.getElementById('people').value;
+billInput.addEventListener("input", billInputFun);
+peopleInput.addEventListener("input", peopleInputFun);
+tips.forEach(function (val) {
+    val.addEventListener("click", handleClick);
+})
+tipCustom.addEventListener('input', tipInputFun);
+resetBtn.addEventListener("click", reset)
 
-    if(bill == '' | tip == 0){
-        alert("Preencha os valores")
-        return;
+billInput.value = '0.0';
+peopleInput.value = '1';
+tipPerPerson.innerHTML = "$" + (0.0).toFixed(2);
+totalPerPerson.innerHTML = "$" + (0.0).toFixed(2);
+
+let billValue = 0.0;
+let peopleValue = 1;
+let tipValue = 0.10;
+
+function billInputFun(){
+    billValue = parseFloat(billInput.value);
+    calculateTip()
+}
+
+function peopleInputFun(){
+    peopleValue = parseFloat(peopleInput.value);
+    calculateTip()
+}
+
+function tipInputFun(){
+    tipValue = parseFloat(tipCustom.value / 100);
+
+    tips.forEach(function (val){
+        val.classList.remove("active-tip");
+    })
+    calculateTip();
+}
+
+function handleClick(event){
+    tips.forEach(function(val) {
+        val.classList.remove("active-tip");
+        if (event.target.innerHTML == val.innerHTML){
+            val.classList.add("active-tip");
+            tipValue = parseFloat(val.innerHTML)/100
+        }
+    })
+    calculateTip()
+}
+
+function calculateTip() {
+    if(peopleValue >= 1){
+        let tipAmount = (billValue * tipValue) / peopleValue;
+        let total = (billValue + tipAmount) / peopleValue;
+        tipPerPerson.innerHTML = "$" + tipAmount.toFixed(2);
+        totalPerPerson.innerHTML = "$" + total.toFixed(2);
     }
+}
 
-    if(numPeople == "" | tip <= 1){
-        numPeople = 1;
-        document.getElementById('each').style.display = "none"
-    } else {
-        document.getElementById('each').style.display = "block"
-    }
-
-    let total = (bill * tip) / numPeople;
-    total = total.toFixed(2);
-
-
-
+function reset () {
+    billInput.value = "0.0";
+    billInputFun()
+    peopleInput.value = "1";
+    peopleInputFun()
+    tipCustom.value = "";
 }
